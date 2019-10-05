e202445fac7c37d16af8463e9877b35a275737a4
JustinH <jharris@escgov.com>
2019-10-05 12:07:54 -0400

    added for loop to loop through the number array after it pushed age and phoneNumber

--- home.js
+++ home.js
@@ -2,8 +2,19 @@ function contactForm() {
     let firstName = document.getElementById('firstName').value;
     let lastName = document.getElementById('lastName').value;
     let fullName = firstName + " " + lastName;
-    console.log(fullName);
     document.getElementById('pushFullName').innerHTML = "You're Full Name Is - " + fullName;
-    let age = document.getElementById('age').value;
-    document.getElementById('pushAge').innerHTML = "You are " + age + " Years old";
+    const age = parseInt(document.getElementById('age').value);
+    //document.getElementById('pushAge').innerHTML = "You are - " + age + " Years old";
+    const phoneNumber = parseInt(document.getElementById('phoneNumber').value);
+    //document.getElementById('pushPhoneNumber').innerHTML = "You \'re phone number is - " + phoneNumber;
+    const numberArray = [];
+    numberArray.push(age, phoneNumber);
+    for (let i = 0; i < numberArray.length; i++) {
+        if (numberArray[i] <= 100) {
+            document.getElementById('pushAge').innerHTML = "You are - " + age + " years old.";
+        }else if (numberArray[i] > 100) {
+            document.getElementById('pushPhoneNumber').innerHTML = "You\'re phone number is - " + phoneNumber;            
+        }
+        
+    }
 }
